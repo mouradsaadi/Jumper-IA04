f73e46e5d5298d77162071a6e56f19a7fe1e9931
mouradsaadi <blackstonedrummer@gmail.com>
2014-05-21 00:28:18 +0200

    create test devices form ajax request

---
+++ JumperIA04/WebContent/js/init.js
@@ -0,0 +1,15 @@
+﻿
+$(document).ready(function() {
+	var sender = new Sender();
+	
+	
+	$("#buttonGet").on("click",function(){
+		data = encodeURIComponent("id_get");
+		sender.send(GET_REQUEST,data,"localhost","8182","hello");
+	});
+	
+	$("#buttonPost").on("click",function(){
+		data = {url:'lequipe.fr',tag : 'sport'};
+		sender.send(POST_REQUEST,data,"localhost","8182","hello");
+	});
+});
---
+++ WebContent/style/style.js
@@ -0,0 +1,9 @@
+/*
+ * gets the width of the specified html object
+ * @function
+ * @global
+ * @param {HtmlObject} pHtmlElement Html object
+ */
+function GET_WIDTH(pHtmlElement) {
+	//return 
+}
